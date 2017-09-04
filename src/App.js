50e46007eb4736ebb65d73e35c0a5dbc05cffb2f
Aline Vianna <aline@alinevianna.com.br>
2017-09-03 21:09:05 -0300

    Finalizando aulas do módulo fundamentos do react

--- src/App.js
+++ src/App.js
@@ -1,12 +1,61 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import ListContacts from "./ListContacts";
+import { Route } from "react-router-dom";
+import CreateContact from "./CreateContact";
+import * as ContactsAPI from "./utils/ContactsAPI";
 
 class App extends Component {
+  state = {
+    contacts: []
+  };
+
+  removeContact = contact => {
+    this.setState(state => ({
+      contacts: state.contacts.filter(c => c.id !== contact.id)
+    }));
+
+    ContactsAPI.remove(contact);
+  };
+
+  componentDidMount() {
+    ContactsAPI.getAll().then(contacts => {
+      this.setState({ contacts });
+    });
+  }
+
+  createContact(contact) {
+    ContactsAPI.create(contact).then(contact => {
+      console.log(contact);
+      this.setState(state => ({
+        contacts: state.contacts.concat([contact])
+      }));
+    });
+  }
+
   render() {
     return (
-      <div>
-        Hello World
+      <div className="app">
+        <Route
+          exact
+          path="/"
+          render={() =>
+            <ListContacts
+              contacts={this.state.contacts}
+              onDeleteContact={this.removeContact}
+            />}
+        />
+        <Route
+          path="/create"
+          render={({ history }) =>
+            <CreateContact
+              onCreateContact={contact => {
+                this.createContact(contact);
+                history.push("/");
+              }}
+            />}
+        />
       </div>
-    )
+    );
   }
 }
 
